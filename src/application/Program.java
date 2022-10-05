0107bbf015a4cd3dab63546f87875f8d474f21a6
Rodrigo Ramos <digojramos@gmail.com>
2022-10-05 09:35:05 -0300

    Criado versão 1

---
+++ src/application/Program.java
@@ -0,0 +1,12 @@
+package application;
+
+public class Program {
+
+	public static void main(String[] args) {
+		
+		System.out.println("DESAFIO GITHUB");
+		System.out.println("VERSÃO 1");
+
+	}
+
+}
